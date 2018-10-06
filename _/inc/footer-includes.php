ca1ce62b7385a06a00262f0e003945e85658546c
Anthony Keller <antkeller1@gmail.com>
2018-10-05 17:22:11 -0700

    Update post filter links and add social media addthis js

--- _/inc/footer-includes.php
+++ _/inc/footer-includes.php
@@ -5,10 +5,12 @@
         if ($host == 'localhost' || $host == 'gnu-blog.test' || $host == 'gnu1516.staging.wpengine.com') {
             // we're on dev, so include the JavaScript individually for easier debugging
             include 'footer-scripts.php';
+            echo '<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b998e8e29343c88"></script>' . "\n";
         } else {
             // include script version for file versioning on production environment
             include_once 'script-version.php';
             // if production, provide the compiled and uglified JS files
             echo '<script type="text/javascript" src="' . get_template_directory_uri() . '/_/js/gnublog.footer.min.js?v=' . $GLOBALS['SCRIPT_VERSION'] . '"></script>' . "\n";
+            echo '<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b998e8e29343c88"></script>' . "\n";
         }
     ?>
--- _/inc/modules/post-filters.php
+++ _/inc/modules/post-filters.php
@@ -1,18 +1,18 @@
 <section class="post-filters">
   <div id="filters" class="filters">
     <ul class="filter-list">
-      <li><a href="/" class="filter-link main all">All Posts</a></li>
+      <li><a href="/blog/" class="filter-link main all">All Posts</a></li>
       <li>|</li>
-      <li><a href="/category/team/mens/" class="filter-link mens">Men's</a></li>
+      <li><a href="/blog/category/team/mens/" class="filter-link mens">Men's</a></li>
       <li>|</li>
-      <li><a href="/category/team/womens/" class="filter-link womens">Women's</a></li>
+      <li><a href="/blog/category/team/womens/" class="filter-link womens">Women's</a></li>
       <li>|</li>
-      <li><a href="/category/events/" class="filter-link events">Events</a></li>
+      <li><a href="/blog/category/events/" class="filter-link events">Events</a></li>
       <li>|</li>
-      <li><a href="/category/art/" class="filter-link art">Art</a></li>
+      <li><a href="/blog/category/art/" class="filter-link art">Art</a></li>
       <li>|</li>
       <!-- <li><a href="/#videos" class="filter-link main videos">Videos</a></li> -->
-      <li><a href="<?php echo esc_url(get_post_format_link('video')); ?>" class="filter-link main videos">Videos</a></li>
+      <li><a href="/blog/type/video/" class="filter-link main videos">Videos</a></li>
     </ul>
   </div><!-- .filters -->
 </section>
